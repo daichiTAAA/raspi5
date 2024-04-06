fcae548bb30b01e8e765af76be3ad22005b5b231
DaiichiroNoda <69710300+donachii@users.noreply.github.com>
2024-04-06 11:24:05 +0900

    WSL2でk3s-agentを実行するための手順を記載した

--- docs/0030_要素技術/0090_K3s/Forwarding-WslPorts.ps1
+++ docs/0030_要素技術/0090_K3s/Forwarding-WslPorts.ps1
@@ -1,4 +1,4 @@
-$wslIp = (wsl hostname -I -split ' ')[0]
+$wslIp = (-split (wsl hostname -I))[0]
 $ports = @(2379, 2380, 6443, 8472, 10250, 10010, 10248, 10249, 30000, 30001, 30002, 30003, 30004, 30005, 30006, 30007, 30008, 30009, 30010, 30011, 30012, 30013, 30014, 30015, 30016, 30017, 30018, 30019, 30020, 30021, 30022, 30023, 30024, 30025, 30026, 30027, 30028, 30029, 30030) # ポートフォワーディングを設定するポートのリスト
 
 # 既存のポートフォワーディングルールを削除
@@ -7,7 +7,7 @@ netsh interface portproxy reset
 foreach ($port in $ports) {
     # 新しいポートフォワーディングルールを追加
     netsh interface portproxy add v4tov4 listenport=$port listenaddress=0.0.0.0 connectport=$port connectaddress=$wslIp
-    Write-Output "Port $port forwarding to $wslIp:$port"
+    Write-Output "Port $port forwarding to ${wslIp}:$port"
 }
 
 # Windowsファイアウォールでのルール設定（すでに存在する場合はスキップ）
