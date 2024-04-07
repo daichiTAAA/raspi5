e20db55142ae42c192217921c48871ec1dd9f38c
DaiichiroNoda <69710300+donachii@users.noreply.github.com>
2024-04-07 18:19:25 +0900

    Kafka、Shcema Registry、AKHQ実行用のdocker composeを作成した。CSIカメラから画像を取得しKafkaに送信するコードを作成した。

---
+++ src/csicam/csicam/produce_image.py
@@ -0,0 +1,84 @@
+import sys
+import time
+import socket
+from datetime import datetime
+from io import BytesIO
+
+sys.path.append("/usr/lib/python3/dist-packages")
+print(sys.path)
+
+from picamera2 import Picamera2, Preview
+from confluent_kafka import Producer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+
+
+def delivery_report(err, msg):
+    if err is not None:
+        print("Message delivery failed: {}".format(err))
+    else:
+        print("Message delivered to {} [{}]".format(msg.topic(), msg.partition()))
+
+
+# Kafka設定
+bootstrap_servers = "192.168.0.104:9092"
+topic = "image-data"
+
+# Schema Registry設定
+schema_registry_url = "http://192.168.0.104:8081"
+schema_registry_client = SchemaRegistryClient({"url": schema_registry_url})
+
+# スキーマ定義
+schema_str = """
+{
+   "namespace": "csicam.image",
+   "name": "Value",
+   "type": "record",
+   "fields" : [
+     {
+       "name" : "image",
+       "type" : "bytes"
+     },
+     {
+       "name" : "timestamp",
+       "type" : "string"
+     },
+     {
+       "name" : "ip_address",
+       "type" : "string"
+     }
+   ]
+}
+"""
+
+avro_serializer = AvroSerializer(schema_registry_client, schema_str)
+
+producer_conf = {"bootstrap.servers": bootstrap_servers}
+producer = Producer(producer_conf)
+
+picam2a = Picamera2(0)
+picam2a.start_preview(Preview.NULL)
+
+while True:
+    img_buffer = BytesIO()
+    picam2a.capture_file(img_buffer)
+    img_bytes = img_buffer.getvalue()
+
+    timestamp = datetime.now().isoformat()
+    ip_address = socket.gethostbyname(socket.gethostname())
+
+    # メッセージ作成
+    message = {"image": img_bytes, "timestamp": timestamp, "ip_address": ip_address}
+
+    # シリアライズ
+    producer.produce(
+        topic=topic,
+        value=avro_serializer(message, schema_str),
+        callback=delivery_report,
+    )
+
+    producer.flush()
+
+    time.sleep(1)
+
+picam2a.stop()
