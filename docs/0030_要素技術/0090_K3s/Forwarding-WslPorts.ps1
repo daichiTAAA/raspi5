7168e250330f162001a79257f4abb448a1199cc0
DaiichiroNoda <69710300+donachii@users.noreply.github.com>
2024-04-07 12:25:45 +0900

    WSL2でk3s-agentを実行するための手順を記載した

--- docs/0030_要素技術/0090_K3s/Forwarding-WslPorts.ps1
+++ docs/0030_要素技術/0090_K3s/Forwarding-WslPorts.ps1
@@ -1,5 +1,5 @@
 $wslIp = (-split (wsl hostname -I))[0]
-$ports = @(2379, 2380, 6443, 8080, 8472, 10250, 10010, 10248, 10249, 30000, 30001, 30002, 30003, 30004, 30005, 30006, 30007, 30008, 30009, 30010, 30011, 30012, 30013, 30014, 30015, 30016, 30017, 30018, 30019, 30020, 30021, 30022, 30023, 30024, 30025, 30026, 30027, 30028, 30029, 30030)
+$ports = @(2379, 2380, 5001, 6443, 8080, 8472, 10250, 51820, 51821)
 
 # 既存のポートフォワーディングルールを削除し再設定する
 netsh interface portproxy reset
@@ -11,13 +11,23 @@ foreach ($port in $ports) {
 
 
 if (Get-Command -Name Remove-NetFirewallRule -ErrorAction SilentlyContinue) {
-    Get-NetFirewallRule -DisplayName "WSL2 Port Forwarding Rule for Port *" | Remove-NetFirewallRule
+    Get-NetFirewallRule -DisplayName "WSL2 Inbound Rule for Port *" | Remove-NetFirewallRule
+} else {
+    Write-Warning "既存のファイアウォールルールを削除する。もしスクリプトから実行できない場合、管理者権限で立ち上げたPowershellウィンドウでSet-ExecutionPolicy RemoteSigned Processを実行して権限設定してから実行する必要がある"
+}
+if (Get-Command -Name Remove-NetFirewallRule -ErrorAction SilentlyContinue) {
+    Get-NetFirewallRule -DisplayName "WSL2 Outbound Rule for Port *" | Remove-NetFirewallRule
 } else {
     Write-Warning "既存のファイアウォールルールを削除する。もしスクリプトから実行できない場合、管理者権限で立ち上げたPowershellウィンドウでSet-ExecutionPolicy RemoteSigned Processを実行して権限設定してから実行する必要がある"
 }
 
+
 foreach ($port in $ports) {
-    $ruleName = "WSL2 Port Forwarding Rule for Port $port"
-    New-NetFirewallRule -DisplayName $ruleName -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow -Profile Private
-    Write-Output "Firewall rule added for port $port"
+    $ruleNameInbound = "WSL2 Inbound Rule for Port $port"
+    New-NetFirewallRule -DisplayName $ruleNameInbound -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow -Profile Private
+    Write-Output "Inbound firewall rule added for port $port"
+
+    $ruleNameOutbound = "WSL2 Outbound Rule for Port $port"
+    New-NetFirewallRule -DisplayName $ruleNameOutbound -Direction Outbound -LocalPort $port -Protocol TCP -Action Allow -Profile Private
+    Write-Output "Outbound firewall rule added for port $port"
 }
