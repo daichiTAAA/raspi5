0225e3be57d64a21a8df2dfd7a81ba5590736580
DaiichiroNoda <69710300+donachii@users.noreply.github.com>
2024-05-26 22:19:48 +0900

    クラウドのAPIを修正した #18

--- src/cloud/wss_stream/api/models/usercamera.py
+++ src/cloud/wss_stream/api/models/usercamera.py
@@ -4,7 +4,7 @@ from api.db import Base
 
 
 class UserCamera(Base):
-    __tablename__ = "users"
+    __tablename__ = "usercameras"
     user_id = Column(String, primary_key=True)
     camera_id = Column(String, primary_key=True)
     rtsp_url = Column(String)
